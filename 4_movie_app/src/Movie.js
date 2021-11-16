1616992115b1b7600456cce3da81e0e17b0b0727
unain-dev <daeun84366@gmail.com>
2021-11-16 23:16:06 +0900

    4_movie_app devide Movie component

---
+++ 4_movie_app/src/Movie.js
@@ -0,0 +1,25 @@
+import propTypes from "prop-types";
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: propTypes.string.isRequired,
+  title: propTypes.string.isRequired,
+  summary: propTypes.string.isRequired,
+  genres: propTypes.arrayOf(propTypes.string).isRequired,
+};
+
+export default Movie;
