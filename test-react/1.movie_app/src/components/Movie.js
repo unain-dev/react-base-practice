e34b55ea3b350faef6250b23cf227843a4232638
unain-dev <daeun84366@gmail.com>
2021-12-08 15:52:29 +0900

    apply styled-components in Movie.js

--- test-react/1.movie_app/src/components/Movie.js
+++ test-react/1.movie_app/src/components/Movie.js
@@ -1,24 +1,83 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Image = styled.img`
+  position: relative;
+  max-width: 150px;
+  width: 100%;
+  margin-right: 30px;
+  box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
+    0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+`;
+
+const Text = styled.h2`
+  margin: 0;
+  font-weight: 300;
+  text-decoration: none;
+`;
+
+const Title = styled(Text)`
+  Link {
+    margin-bottom: 5px;
+    font-size: 24px;
+    color: #2c2c2c;
+    text-decoration: none;
+  }
+`;
+
+const Year = styled(Text)`
+  margin-right: 10px;
+  font-size: 14px;
+`;
+
+const Genres = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 5px 0px;
+  li {
+    margin-right: 10px;
+    font-size: 14px;
+  }
+`;
+
+const MovieStyle = styled.div`
+  background-color: white;
+  margin-bottom: 70px;
+  font-weight: 300;
+  padding: 20px;
+  border-radius: 5px;
+  color: #adaeb9;
+  display: grid;
+  grid-template-columns: minmax(150px, 1fr) 2fr;
+  grid-gap: 20px;
+  text-decoration: none;
+  color: inherit;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+    0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+`;
 
 function Movie({ id, year, coverImg, title, summary, genres, rating }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
+    <MovieStyle>
+      <Image src={coverImg} alt={title} />
       <div>
-        <h2>
+        <Title>
           <Link to={`/movie/${id}`}>{title}</Link>
-        </h2>
-        <h3>{year}</h3>
+        </Title>
+        <Text>rating : {rating}</Text>
+        <Year as="h3">{year}</Year>
         <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
-        <ul>
+        <Genres>
           {genres.map((g) => (
             <li key={g}>{g}</li>
           ))}
-        </ul>
+        </Genres>
       </div>
-      <hr />
-    </div>
+    </MovieStyle>
   );
 }
 
